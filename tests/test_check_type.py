4c8320427cf050da9cbd140715f40087abc29b64
JHWu92 <jhwu92@gmail.com>
2017-06-12 20:08:41 -0400

    UPD - pkg import

--- tests/test_check_type.py
+++ tests/test_check_type.py
@@ -1,15 +1,15 @@
 # coding=utf-8
 import unittest
-from wjh_other import check_type
+from wKit.utility import check_dtype as chk
 
 
 class TypeTests(unittest.TestCase):
     def testCheckTypes(self):
         list_int = [1, 2, 3, 4, 5]
         list_with_str = ['1', 2, 3]
-        all_int = check_type.check_type(list_int, int)
-        with_none_int = check_type.check_type(list_with_str, int)
-        int_or_str = check_type.check_type(list_with_str, (int, str))
+        all_int = chk.chk(list_int, int)
+        with_none_int = chk.chk(list_with_str, int)
+        int_or_str = chk.chk(list_with_str, (int, str))
         self.failUnless(all_int)
         self.failIf(with_none_int)
         self.failUnless(int_or_str)
@@ -17,40 +17,40 @@ class TypeTests(unittest.TestCase):
     def testAllInt(self):
         intable = [1, 2, '3', 4, '5,00', -1, '-10,000']
         digitable_not_intable = intable+['1.', 1.]
-        self.failUnless(check_type.all_int_able(intable))
-        self.failIf(check_type.all_int_able(digitable_not_intable))
+        self.failUnless(chk.all_int_able(intable))
+        self.failIf(chk.all_int_able(digitable_not_intable))
 
     def testIsInt(self):
         # test cases for numeric type
-        self.failUnless(check_type.int_able(1))
-        self.failIf(check_type.int_able(1.1))
-        self.failIf(check_type.int_able(1.))
-        self.failUnless(check_type.int_able(-1))
-        self.failIf(check_type.int_able(-1.1))
+        self.failUnless(chk.int_able(1))
+        self.failIf(chk.int_able(1.1))
+        self.failIf(chk.int_able(1.))
+        self.failUnless(chk.int_able(-1))
+        self.failIf(chk.int_able(-1.1))
 
         # test cases for string
-        self.failUnless(check_type.int_able('1'))
-        self.failIf(check_type.int_able('1.1'))
-        self.failIf(check_type.int_able('1.'))
-        self.failUnless(check_type.int_able('1,000'))
-        self.failUnless(check_type.int_able('1,00,0'))
-        self.failIf(check_type.int_able('1,00,0.0'))
+        self.failUnless(chk.int_able('1'))
+        self.failIf(chk.int_able('1.1'))
+        self.failIf(chk.int_able('1.'))
+        self.failUnless(chk.int_able('1,000'))
+        self.failUnless(chk.int_able('1,00,0'))
+        self.failIf(chk.int_able('1,00,0.0'))
 
     def testIsDigit(self):
         # test cases for numeric type
-        self.failUnless(check_type.digit_able(1))
-        self.failUnless(check_type.digit_able(1.1))
-        self.failUnless(check_type.digit_able(-1))
-        self.failUnless(check_type.digit_able(-1.1))
+        self.failUnless(chk.digit_able(1))
+        self.failUnless(chk.digit_able(1.1))
+        self.failUnless(chk.digit_able(-1))
+        self.failUnless(chk.digit_able(-1.1))
 
         # test cases for string
-        self.failUnless(check_type.digit_able('1'))
-        self.failUnless(check_type.digit_able('-1'))
-        self.failUnless(check_type.digit_able('1.1'))
-        self.failUnless(check_type.digit_able('1,000'))
-        self.failUnless(check_type.digit_able('1,00,0'))
-        self.failUnless(check_type.digit_able('1,00,0.0'))
-        self.failUnless(check_type.digit_able('-1,00,0.0'))
+        self.failUnless(chk.digit_able('1'))
+        self.failUnless(chk.digit_able('-1'))
+        self.failUnless(chk.digit_able('1.1'))
+        self.failUnless(chk.digit_able('1,000'))
+        self.failUnless(chk.digit_able('1,00,0'))
+        self.failUnless(chk.digit_able('1,00,0.0'))
+        self.failUnless(chk.digit_able('-1,00,0.0'))
 
 
 def main():
