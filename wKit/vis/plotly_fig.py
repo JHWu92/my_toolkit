7e5a8d2a3b009beb34f14e867a8da05d93630186
JHWu92 <jhwu92@gmail.com>
2017-06-14 14:39:03 -0400

    ADD plotly_fig/bar definition

--- wKit/vis/plotly_fig.py
+++ wKit/vis/plotly_fig.py
@@ -2,14 +2,14 @@
 from plotly import graph_objs as go
 
 
-def box(arrs, names=None, title=None, orientation='v', reverse=False):
+def box(arrs, box_names=None, title=None, orientation='v', reverse=False):
     """
     boxplot with plotly
 
     Parameters
     ----------
     arrs: list of arr. Each arr is one box.
-    names: list. Names for each box.
+    box_names: list. Names for each box.
     title: str. title of the graph. Default None
     orientation: {'v', 'h'}, default v(verticle)
     reverse: boolean. Default False.
@@ -22,15 +22,15 @@ def box(arrs, names=None, title=None, orientation='v', reverse=False):
     """
 
     size = len(arrs)
-    if names is None:
-        names = ['box %d' % i for i in range(size)]
-    assert len(names) == size, 'len of arrs dosent match with len of names'
+    if box_names is None:
+        box_names = ['box %d' % i for i in range(size)]
+    assert len(box_names) == size, 'len of arrs dosent match with len of names'
 
     order = range(size) if not reverse else range(size - 1, -1, -1)
     if orientation == 'h':
-        data = [go.Box(x=arrs[i], name=names[i]) for i in order]
+        data = [go.Box(x=arrs[i], name=box_names[i]) for i in order]
     elif orientation == 'v':
-        data = [go.Box(y=arrs[i], name=names[i]) for i in order]
+        data = [go.Box(y=arrs[i], name=box_names[i]) for i in order]
     else:
         raise ValueError("orienation should be in {'v', 'h'}")
 
@@ -71,3 +71,27 @@ def hist(arrs, names=None, title=None, orientation='v'):
     layout = go.Layout(title=title)
     fig = go.Figure(data=data, layout=layout)
     return fig
+
+
+def bar(arrs, labels=None, errs=None, groups_name=None, title=None, orientation='v', reverse=False):
+    """
+    boxplot with plotly
+
+    Parameters
+    ----------
+    arrs: list of arrs. [[group1], [group2], ...].
+    labels: [[labels for group1], [labels2], ...], default None
+    errs: [[err for group1], [err2], ...], default None
+    groups_name: list. [name1, name2,...], default None
+    title: str. title of the graph. Default None
+    orientation: {'v', 'h'}, default v(verticle)
+    reverse: boolean. Default False.
+        If True:  'v': up->down; 'h': right->left
+        If False: 'v': down->up; 'h': left->right
+
+    Return
+    ----------
+    plotly.graph_objs.Figure for iplot/plot
+    """
+    return
+
